59cd434686b6e5b95001403993c5bd60bd8d4440
Hakob Tumanyan <jamestumanyan@Jamess-MacBook-Air.local>
2020-09-10 22:44:43 +0400

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+
+const app = express()
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
+app.use(cors())
+
+dotenv.config()
+
+app.get('/books', (req, res, next) => {
+    return res.json([
+        {
+            name : "The brief history of time",
+        },
+        {
+            name : "The brief history of time",
+        },
+        {
+            name : "The brief history of time",
+        }
+    ])
+})
+
+app.listen(process.env.SERVER_PORT || 3000, (err, res) => {
+    if(!err) {
+        console.log('ok')
+    }
+})
